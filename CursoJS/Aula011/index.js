28ed0eaf7dc0ca4097d439b5ca4abdfa2018db1e
EstudanteErick <silvermental50@gmail.com>
2024-11-19 23:34:08 -0400

    esto quase terminado o curso de javascript so falta 14 aulas :D

---
+++ CursoJS/Aula011/index.js
@@ -0,0 +1,9 @@
+var país = 'EUA'
+console.log (`Vivendo em ${país}`)
+if (país == 'brasil') {
+    console.log ('Você esta morando no Brasil!')
+}
+
+else {
+    console.log ('Você é Estrangeinho!')
+}
---
+++ CursoJS/Aula09/index.js
@@ -0,0 +1,6 @@
+var vel = 80.00
+console.log(`Seu carro atigiu a velocidade ${vel}km/h`)
+if(vel > 60) {
+    console.log (`Você ultrapassou a velocidade permitida! MULTADO!`) //condição simples
+}
+console.log (`Dirija sempre usando o cinto de segurança!`)
