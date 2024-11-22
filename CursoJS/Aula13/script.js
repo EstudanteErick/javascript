94fd9ca09bee7e677e66dcdee538a596e9a8fd15
EstudanteErick <silvermental50@gmail.com>
2024-11-21 23:16:30 -0400

    ja terminei o exercicio das horas

--- CursoJS/Aula13/script.js
+++ CursoJS/Aula13/script.js
@@ -1,25 +1,33 @@
 function carregar() {
-    var msg = window.document.getElementById ('msg')
-    var img = window.document.getElementById ('imagem')
 
+    var msg = window.document.getElementById('msg') 
+    var img = window.document.getElementById('imagem') 
+    
     var data = new Date()
+    //var hora = 19
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
 
-    var hora = 22
-
-    msg.innerHTML = `Agora são ${hora} horas`
     if (hora >= 0 && hora < 12) {
-        // BOM DIA
-        img.src = 'amanhã.png'
-    }
+    // BOM DIA!
 
-    else if (hora >= 12 && hora < 18) {
-        //BOA TARDE
-        img.src = 'tarde.png'
-    }
+    img.src = 'amanhã.png'
 
-    else {
-        //BOA NOITE
-        img.src = 'noite.png'
+    document.body.style.background = '#e2cd9f'
+
+    } else if (hora >= 12 && hora <= 18) {
+    // BOA TARDE!
+
+    img.src = 'tarde.png'
+    document.body.style.background = '#b9846f'
+    } else {
+    // BOA NOITE!
+
+    img.src='noite.png'
+    document.body.style.background = '#515154'
     }
 }
 
+    
+
+    
