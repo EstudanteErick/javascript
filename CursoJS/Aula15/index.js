d5bc755335313c9241224088e477918795a0c0e8
EstudanteErick <silvermental50@gmail.com>
2024-11-24 23:34:23 -0400

    terminei a Aula14

---
+++ CursoJS/Aula15/index.js
@@ -0,0 +1,13 @@
+var c = 1
+do {
+    console.log (`A bomba vai ser lançada em ${c}`)
+    c++
+} while (c <= 10)
+
+    /*
+    var c = 1
+    while (c <= 6) {
+    console.log (`Passo ${c}`)
+    c++ significa c = c + 1 
+    }
+    */
---
+++ CursoJS/Aula16/ambiente.js
@@ -0,0 +1,5 @@
+console.log ('Vai Começar...')
+for(var c = 1;c <= 3;c++ ) {
+    console.log ('Você e feio')
+}
+console.log ('FIM')
